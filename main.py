f266bdeaa1bb73192e6a9303d559985076fa0279
Sammybams <samuelbamgbola@gmail.com>
2024-09-25 11:44:18 +0100

    Setting up repo for streamlit and required python packages.

--- main.py
+++ main.py
@@ -1,1 +1,38 @@
 import streamlit as st
+st.set_page_config(page_title="Speak-To-Docs", page_icon="📝", layout="wide", initial_sidebar_state="expanded")
+
+import os
+
+# Langchain components
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+
+# Add OpenAI library
+import openai
+
+# Get Configuration Settings
+from dotenv import load_dotenv
+load_dotenv()
+
+# Configure OpenAI API using Azure OpenAI
+openai.api_key = os.getenv("API_KEY")
+openai.api_base = os.getenv("ENDPOINT")
+openai.api_type = "azure"  # Necessary for using the OpenAI library with Azure OpenAI
+openai.api_version = os.getenv("OPENAI_API_VERSION")  # Latest / target version of the API
+
+# Implementation
+from langchain.embeddings import OpenAIEmbeddings
+
+# OpenAI Settings
+model_deployment = "text-embedding-ada-002"
+# SDK calls this "engine", but naming it "deployment_name" for clarity
+
+model_name = "text-embedding-ada-002"
+
+openai_embeddings: OpenAIEmbeddings = OpenAIEmbeddings(
+    openai_api_version = os.getenv("OPENAI_API_VERSION"), openai_api_key = os.getenv("API_KEY"),
+    openai_api_base = os.getenv("ENDPOINT"), openai_api_type = "azure"
+)
+
+# LLM - Azure OpenAI
+llm = ChatOpenAI(temperature = 0.3, openai_api_key = os.getenv("API_KEY"), openai_api_base = os.getenv("ENDPOINT"), model_name="gpt-35-turbo", engine="Voicetask")
